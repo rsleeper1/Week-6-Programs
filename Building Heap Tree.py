73fc0517bb366b8653cc3c1fad94a438c62cd25a
rsleeper1 <39035524+rsleeper1@users.noreply.github.com>
2018-08-10 18:17:14 -0500

    Updates

--- Building Heap Tree.py
+++ Building Heap Tree.py
@@ -1,33 +1,26 @@
 #Build Heap Method
 #Ryan Sleeper
+#This program generates a random list of integers and then creates a heap tree
+#from the numbers in the list.
 
 import random 
 
-class BinaryHeapTree:
+class BinaryHeapTree:          #This class creates a binary heap tree given a list.
     def __init__(self):
         self.size = 0
         self.heapList = [0]
         
-    def insertNode(self, node):
-        self.heapList.append(node)
-        self.size = self.size + 1
-        self.moveUp(self.size)
         
-    def moveUp(self, node):
-        while node // 2 > 0:
-          if self.heapList[node] < self.heapList[node // 2]:
-             self.heapList[node // 2], self.heapList[node] = self.heapList[node], self.heapList[node // 2]
-          node = node // 2
-          
-    def moveDown(self, node):
-        while (node * 2) <= self.size:
+    def moveDown(self, node):                   #This method calls the minChild method and then swaps the nodes if the 
+        while (node * 2) <= self.size:          #current node is greater than the smallest node. 
             smallestNode = self.minChild(node)
             if self.heapList[node] > self.heapList[smallestNode]:
                 self.heapList[node], self.heapList[smallestNode] = self.heapList[smallestNode], self.heapList[node]
             node = smallestNode
 
-    def minChild(self, node):
-        if node * 2 + 1 > self.size:
+
+    def minChild(self, node):                  #This method finds the smallest child in the tree on the current
+        if node * 2 + 1 > self.size:           #level of the tree it is on and then returns it to the moveDown method.
             return node * 2
         else:
             if self.heapList[node * 2] < self.heapList[node * 2 + 1]:
@@ -35,17 +28,18 @@ class BinaryHeapTree:
             else:
                 return node * 2 + 1
             
-    def buildHeap(self, list):
-        level = len(list) // 2
-        self.size = len(list)
+            
+    def buildHeap(self, list):             #This method takes a list of integers and produces a heap tree by 
+        level = len(list) // 2             #first initializing the heap list and then using the moveDown method
+        self.size = len(list)              #to sort the list.
         self.heapList = [0] + list[:]
         while (level > 0):
             self.moveDown(level)
             level = level - 1
 
     
-def main():
-    randomNumberList = random.sample(range(100), 20)
+def main():                                               #The main function generates a random list of integers and then 
+    randomNumberList = random.sample(range(100), 20)      #creats a binary heap tree and prints out the results.
     print(randomNumberList)
     myHeapTree = BinaryHeapTree()
     myHeapTree.buildHeap(randomNumberList)
--- My Heap Tree.py
+++ My Heap Tree.py
@@ -1,29 +1,31 @@
 #Binary Heap Tree
 #Ryan Sleeper
+#This program generates a random list of integers and then produces a heap tree by adding
+#the numbers to the tree one at a time and sorting them as we append.
 
 import random 
 
-class BinaryHeapTree:
+class BinaryHeapTree:             #This class creates a heap tree given a list of numbers.
     def __init__(self):
         self.size = 0
         self.heapList = [0]
         
-    def insertNode(self, node):
-        self.heapList.append(node)
+    def insertNode(self, node):      #This method inserts a new node into the tree and then calls the moveUp 
+        self.heapList.append(node)   #method to sort it through out the tree.
         self.size = self.size + 1
         self.moveUp(self.size)
         
-    def moveUp(self, node):
+    def moveUp(self, node):          #This method sorts the tree as each number gets added to it.
         while node // 2 > 0:
           if self.heapList[node] < self.heapList[node // 2]:
              self.heapList[node // 2], self.heapList[node] = self.heapList[node], self.heapList[node // 2]
           node = node // 2
           
     
-def main():
-    randomNumberList = random.sample(range(100), 20)
-    print(randomNumberList)
-    myHeapTree = BinaryHeapTree()
+def main():                                             #The main function generates a random list of numbers and then creates a 
+    randomNumberList = random.sample(range(100), 20)    #heap tree and adds the numbers from the list to the heap tree one at a time.
+    print(randomNumberList)                             #I have the program print out each iteration so the user can see how the tree
+    myHeapTree = BinaryHeapTree()                       #is created.
     for number in range(len(randomNumberList)):
         myHeapTree.insertNode(randomNumberList[number])
         print(myHeapTree.heapList)
